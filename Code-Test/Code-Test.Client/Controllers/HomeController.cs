92d2c18bdae0759ea84c424764b2c10dbdbaadf1
Erik Polsean <polseane9067@my.uwstout.edu>
2016-12-10 17:35:49 -0500

    Add MVC

---
+++ Code-Test/Code-Test.Client/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Code_Test.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Home()
+        {
+            return View();
+        }
+
+        public PartialViewResult Calculator()
+        {
+            return PartialView();
+        }
+    }
+}
